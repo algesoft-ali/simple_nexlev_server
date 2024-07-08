ffb73a33b8615ba60be014150805584fe0f0970b
algesoft-ali <mohammad.ali@algesoft.com>
2024-07-08 23:44:38 +0600

    user registration & get all user route added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req } from "@nestjs/common";
+import { Controller, Get, HttpException, Post, Req } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { Request } from "express";
 
@@ -7,16 +7,35 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   // ------ Register User
-  @Post()
-  registerUser(@Req() req: Request) {
-    const {} = req.body;
-    const data = this.userService.registerUser(req.body);
+  @Post("register")
+  async registerUser(@Req() req: Request) {
+    try {
+      const data = await this.userService.registerUser(req.body);
 
-    return {
-      success: true,
-      message: "User Registered Successfully",
-      data,
-    };
+      return {
+        success: true,
+        message: "User Registered Successfully",
+        data,
+      };
+    } catch (error) {
+      throw new HttpException(error, 500);
+    }
   }
   // ------ Login User
+
+  // ------ Get All User
+  @Get()
+  async getAllUser() {
+    try {
+      const data = await this.userService.getAllUser();
+      return {
+        success: true,
+        message: "Users retrieved Successfully",
+        data,
+      };
+    } catch (error) {
+      throw new HttpException(error, 500);
+    }
+  }
+  // ------ Get One User
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,36 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma.service";
 import { User, Prisma } from "@prisma/client";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async registerUser(data: Prisma.UserCreateInput): Promise<User> {
+    const inputtedPassword = data?.password;
+    // ------ hash the password using bcrypt
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(inputtedPassword, salt);
+    data.password = hashedPassword;
+
     return this.prisma.user.create({
       data,
+    });
+  }
+
+  async getAllUser(): Promise<Omit<User, "password">[]> {
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        role: true,
+        email: true,
+        password: false,
+        address: true,
+        createdAt: true,
+        updatedAt: true,
+      },
     });
   }
 }
