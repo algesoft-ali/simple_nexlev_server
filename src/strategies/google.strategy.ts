03dde07e53bdbecfaeb455294bed662f029adc34
algesoft-ali <mohammad.ali@algesoft.com>
2024-07-11 20:04:52 +0600

    callback url updated

--- src/strategies/google.strategy.ts
+++ src/strategies/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, "google") {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/user/google/callback",
+      callbackURL: `${process.env.SERVER_URL}/user/google/callback`,
       scope: ["email", "profile"],
     });
   }
